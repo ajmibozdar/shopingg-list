ec4ec0f8714987b4282256c49e63cd4462ba8f8b
ajmal <ajmalebozdar@gmail.com>
2023-12-23 21:26:55 -0800

    Add files via upload
    added files via uploading directly from folder.

---
+++ main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+
+    <h1>this is my first project on react</h1>
+    <p>
+      i started a making projects to become a software enginer to help comunity
+    </p>
+  </React.StrictMode>
+);
